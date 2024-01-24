1bf1b7ace70b5fd189e005fc4040ff91c366ce4d
unknown7761 <ismoilzanovnasrullo@gmail.com>
2024-01-24 17:57:04 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Насрулло Исмоилжонов on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
